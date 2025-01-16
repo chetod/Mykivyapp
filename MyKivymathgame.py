9f6a905990dcf7cfdcfd1223dd79c69eec5813e4
singhanat <ainghast@gmail.com>
2025-01-16 22:12:44 +0700

    Chage text title to .kv file

--- MyKivymathgame.py
+++ MyKivymathgame.py
@@ -1,22 +1,15 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty
 
-class NumberInputApp(App):
+class MathGame(BoxLayout):
+    pass
+class MathGameApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
-        label = Label(text='Input number:', size_hint_y=None, height=30)
-        self.number_input = TextInput(
-            multiline=False,
-            input_type='number',  
-            input_filter='float',  
-            size_hint_y=None,
-            height=40
-        )
-        layout.add_widget(label)
-        layout.add_widget(self.number_input)
-        return layout
+        return MathGame()
 
 if __name__ == '__main__':
-    NumberInputApp().run()
+    MathGameApp().run()
