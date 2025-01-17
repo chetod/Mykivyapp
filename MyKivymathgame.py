4355054c7f5bebefbaec50175770bccaa6b264bb
singhanat <ainghast@gmail.com>
2025-01-17 13:20:49 +0700

    add start and setting button

--- MyKivymathgame.py
+++ MyKivymathgame.py
@@ -6,12 +6,25 @@ from kivy.uix.textinput import TextInput
 from kivy.properties import NumericProperty, StringProperty, BooleanProperty
 
 class MathGame(BoxLayout):
-    score = NumericProperty(0)#save scores 
-    question = StringProperty("Press Start to Begin") # Show question only string
-    game_active = BooleanProperty(False) # to add start button
-    
-    def check_answer(self, answer_text): # fucntion check
-        self.question = "This is where the answer will be checked." 
+    score = NumericProperty(0)
+    question = StringProperty("Press Start to Begin")
+    game_active = BooleanProperty(False)
+
+    def start_game(self):
+        self.game_active = True
+        self.question = "Game Started! Prepare for the first question."
+    def check_answer(self, answer_text):
+        
+        self.question = "This is where the answer will be checked."
+
+    def show_settings(self):
+        
+        print("Settings button clicked!")
+
+    def reset_game(self):
+        self.score = 0
+        self.game_active = False
+        self.question = "Press Start to Begin"
 
 class MathGameApp(App):
     def build(self):
